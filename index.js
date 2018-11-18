8eb4c65e144ef4758664bde843c0849b861fc342
vishal chauhan <vishuchauhan@gmail.com>
2018-11-18 21:35:23 +0530

    v1 to v2
    test

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
-    displayText: speech,
+    fulfillmentText: speech,
     source: "webhook-echo-sample"
   });
 });
